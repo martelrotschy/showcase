aa8c5b13fc5dc54dad07e385ec915de20be62c27
Martel <126117735+martelrotschy@users.noreply.github.com>
2023-03-23 08:43:30 -0700

    Add files via upload

---
+++ showcase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace showcase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
